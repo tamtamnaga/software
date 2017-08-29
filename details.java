87138cec19867a3c52fd94c30f90825d8a1ec395
tamtamnaga <lakshmaiah.ds@gmail.com>
2017-08-29 14:41:16 +0530

    added java file

---
+++ details.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class details
+{
+public static void main(String args[])
+{
+int p;
+Scanner input=new Scanner(System.in);
+System.out.println("hiii naga");
+System.out.println("Enter your phone number");
+p=input.nextInt();
+System.out.println("phone number is" +p);
+}
+}
